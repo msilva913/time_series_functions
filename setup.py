12b30e51a482f353725f29670e589fee2f649d8d
Mario Silva <msilva913@gmail.com>
2020-09-20 10:49:30 -0700

    further changes

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 14:33:04 2020
+
+@author: Administrator
+"""
+
+from setuptools import setup, find_packages
+setup(name='time_series_functions',
+      author='Mario Rafael Silva',
+      version='0.1',
+      packages = find_packages(),
+      )
